93ec2c1aad1ca2abd4f8a9dd94c10140c1a3c894
reihned <reihned@gmail.com>
2015-03-07 14:36:08 -0500

    python specific styles and more functionality

--- card_game.py
+++ card_game.py
@@ -1,3 +1,5 @@
+import 'random'
+
 class Deck(object):
 
     SUITS = ["C", "D", "H", "S"]
@@ -5,9 +7,15 @@ class Deck(object):
 
     def __init__(self):
         self.cards = []
+        self._generate_card()
+
+    def _generate_cards(self):
         for suit in self.SUITS:
             for rank in self.RANKS:
                 card = suit+rank
                 self.cards.append(card)
+
+    def _shuffle_Cards(self):
+
 
 deck = Deck()
